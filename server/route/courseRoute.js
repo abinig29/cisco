7eded03048700aeaa6a15187cb30e3c2015e3362
abinig29 <abinig5@gmail.com>
2023-07-11 22:10:51 +0300

    done with backend

--- server/route/courseRoute.js
+++ server/route/courseRoute.js
@@ -6,11 +6,16 @@ import {
   coverTopic,
   updateCourse,
 } from "../controller/courseController.js";
-
+import { verifyJWT } from "../middleware/verifyToken.js";
+import { verfiyRole } from "../middleware/verifyRole.js";
+import { roles } from "../config/role.js";
 const router = express.Router();
 
-router.route("/").get(getCourses).post(createCourse);
-router.route("/:id").patch(updateCourse).delete(deleteCourse);
-router.route("/:id/coverTopic").patch(coverTopic);
+router.get("/", getCourses);
+router
+  .route("/:id")
+  .patch(verifyJWT, verfiyRole(roles.admin, roles.lecture), updateCourse)
+  .delete(verifyJWT, verfiyRole(roles.admin), deleteCourse);
+router.route("/:id/coverTopic").patch(verifyJWT, coverTopic);
 
 export default router;
--- server/route/userRoute.js
+++ server/route/userRoute.js
@@ -3,12 +3,14 @@ import {
   getUsers,
   updateUser,
   deleteUser,
-  createUser,
 } from "../controller/userController.js";
+import { verifyJWT } from "../middleware/verifyToken.js";
+import { verfiyRole } from "../middleware/verifyRole.js";
+import { roles } from "../config/role.js";
 
 const router = express.Router();
-
-router.route("/").get(getUsers).post(createUser);
+router.use(verifyJWT, verfiyRole(roles.admin));
+router.route("/").get(getUsers);
 router.route("/:id").patch(updateUser).delete(deleteUser);
 
 export default router;
