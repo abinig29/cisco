636e8df0f5a775f566773c4b3c07ab90b53b5b4d
Abel Nigus <abinig5@gmail.com>
2023-11-14 21:51:41 +0300

    touch

--- server/config/corsOption.js
+++ server/config/corsOption.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://cisco-course-registration.netlify.app/"
+  "https://cisco-course-registration.netlify.app/",
+
 ];
 
 const corsOptions = {
