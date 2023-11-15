be67ed0ad3245285e684b0c3861f93bb4a4d5df5
Abel Nigus <abinig5@gmail.com>
2023-11-15 13:53:00 +0300

    done

--- server/config/corsOption.js
+++ server/config/corsOption.js
@@ -1,13 +1,13 @@
 const allowedOrigins = [];
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  // origin: (origin, callback) => {
+  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error("Not allowed by CORS"));
+  //   }
+  // },
   methods: ["GET", "POST", "PATCH ", "DELETE"],
   credentials: true,
   optionsSuccessStatus: 200,
