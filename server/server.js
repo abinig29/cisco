1d3e7f77f3356d25c7a26816dce3d12338da4dac
Abel Nigus <abinig5@gmail.com>
2023-11-15 16:45:54 +0300

    change

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const app = express();
 if ((process.env.NODE_ENV = "development")) {
   app.use(morgan("common"));
 }
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", [
     "https://cisco-course-registration.netlify.app",
