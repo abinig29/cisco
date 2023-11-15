4a039091802ea5e8426e98af0b3392cc01e73339
Abel Nigus <abinig5@gmail.com>
2023-11-15 13:57:27 +0300

    done

--- server/config/corsOption.js
+++ server/config/corsOption.js
@@ -1,13 +1,17 @@
-const allowedOrigins = [];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://cisco-course-registration.netlify.app/",
+  "https://cisco-course-registration.netlify.app/",
+];
 
 const corsOptions = {
-  // origin: (origin, callback) => {
-  //   if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error("Not allowed by CORS"));
-  //   }
-  // },
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PATCH ", "DELETE"],
   credentials: true,
   optionsSuccessStatus: 200,
